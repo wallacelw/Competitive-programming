2ac3993b581208647ea328f7550f947f4d3129e7
Wallace <wallacebtlw@gmail.com>
2024-02-12 14:01:47 -0300

    string refactoring

--- content/data-structures/ordered-set.cpp
+++ content/data-structures/ordered-set.cpp
@@ -1,14 +1,14 @@
 /**
  * Author: Wallace
  * Date: 07/02/2024
- * Description: Set with index operators, implemented by gnu pbds
+ * Description: Set with index operators, implemented by gnu pbds.
  * Remember to compile with gcc!!
  * Time: O(log(N)) but with slow constant
  */
 
 // 0-idx
-// find_by_order(i) -> iterator to elem with index i; O(log(N))
-// order_of_key(i) -> index of key; O(log(N))
+// find_by_order(i) -> iterator to elem with index i
+// order_of_key(val) -> index of key
 
 // Ordered Set
 #include <bits/extc++.h>
