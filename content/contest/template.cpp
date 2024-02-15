f63bf322df3794da162311dcd59a75adcc4ff2f7
Wallace <wallacebtlw@gmail.com>
2024-02-14 22:42:42 -0300

    chrono count time template update

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -21,14 +21,19 @@ mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
 #define debug(a...) cerr<<#a<<": ";for(auto b:a)cerr<<b<<" ";cerr<<endl;
 template<typename... A> void dbg(A const&... a){((cerr<<"{"<<a<<"} "), ...);cerr<<endl;}
 
-const int MAX = 3e5+10;
-const int INF = INT32_MAX;
-const long long MOD = 1e9+7;
-const long long INF = 0x3f3f3f3f3f3f3f3f;
-const long long LLINF = INT64_MAX;
-const long double EPS = 1e-7;
-const long double PI = acos(-1);
+const ll MAX = 3e5+10;
+const ll MOD = 1e9+7;
+const ll INF = 0x3f3f3f3f3f3f3f3f;
+const ll LLINF = INT64_MAX;
+const ld EPS = 1e-7;
+const ld PI = acos(-1);
 
+#include <chrono>
+using namespace std::chrono;
 int32_t main(){ sws;
-
+    auto start = high_resolution_clock::now();
+    // function here
+    auto stop = high_resolution_clock::now();
+    auto duration = duration_cast<milliseconds>(stop - start);
+    cout << duration.count() << endl;
 }  
