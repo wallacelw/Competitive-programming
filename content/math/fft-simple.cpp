8ebd44aa491006ddc2f197281f791ebfbd760579
Wallace <wallacebtlw@gmail.com>
2024-02-16 00:26:18 -0300

    fft added, kactl math removed

---
+++ content/math/fft-simple.cpp
@@ -0,0 +1,79 @@
+/**
+ * Author: Tiagosf00
+ * Date: 16/02/2024
+ * Description: Computes the product between two polynomials using fft
+ * Time: O(N\log{N})
+ * Status: Tested (https://cses.fi/problemset/result/8483778/)
+ */
+
+
+// #define ld long double
+// const ld PI = acos(-1);
+
+struct num{
+    ld a {0.0}, b {0.0};
+    num(){}
+    num(ld na) : a{na}{}
+    num(ld na, ld nb) : a{na}, b{nb} {}
+    const num operator+(const num &c) const{
+        return num(a + c.a, b + c.b);
+    }
+    const num operator-(const num &c) const{
+        return num(a - c.a, b - c.b);
+    }
+    const num operator*(const num &c) const{
+        return num(a*c.a - b*c.b, a*c.b + b*c.a);
+    }
+    const num operator/(const int &c) const{
+         return num(a/c, b/c);
+    }
+};
+
+void fft(vector<num> &a, bool invert){
+    int n = (int)a.size();
+    for(int i=1,j=0;i<n;i++){
+        int bit = n>>1;
+        for(; j&bit; bit>>=1)
+            j^=bit;
+        j^=bit;
+        if(i<j)
+            swap(a[i], a[j]);
+    }
+    for(int len = 2; len <= n; len <<= 1){
+        ld ang = 2 * PI / len * (invert ? -1 : 1);
+        num wlen(cos(ang), sin(ang));
+        for(int i=0;i<n;i+=len){
+            num w(1);
+            for (int j=0;j<len/2;j++){
+                num u = a[i+j], v = a[i+j+len/2] * w;
+                a[i+j] = u + v;
+                a[i+j+len/2] = u - v;
+                w = w * wlen;
+            }
+        }
+    }
+    if(invert)
+      for(num &x: a)
+          x = x/n;
+
+}
+
+vector<ll> multiply(vector<int> const& a, vector<int> const& b){
+    vector<num> fa(a.begin(), a.end());
+    vector<num> fb(b.begin(), b.end());
+    int n = 1;
+    while(n < int(a.size() + b.size()) )
+        n <<= 1;      
+    fa.resize(n);
+    fb.resize(n);
+    fft(fa, false);
+    fft(fb, false);   
+    for(int i=0;i<n;i++)
+        fa[i] = fa[i]*fb[i];
+    fft(fa, true);
+    vector<ll> result(n);
+    for(int i=0;i<n;i++)
+        result[i] = (ll) round(fa[i].a);
+    while(result.back()==0) result.pop_back();
+    return result;
+}
