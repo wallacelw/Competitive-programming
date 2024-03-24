f08f9d9a67a5dfbe7efbd9fafcf8595e0a006432
Wallace <wallacebtlw@gmail.com>
2024-03-24 16:52:14 -0300

    update dijkstra names

--- content/graph/dijkstra.cpp
+++ content/graph/dijkstra.cpp
@@ -1,8 +1,9 @@
 /**
  * Author: Wallace
- * Date: 14/02/2024
- * Description: By keeping track of the distances sorted using an priority queue,
- * transverse only using the smallest distances.
+ * Date: 24/03/2024
+ * Description: By keeping track of the distances sorted using an priority queue of candidates.
+ * if an edge can reduce the current min distance, insert into the priority queue.
+ * ONLY when the vertice is dequeued and its cost is <= d[u], it is in fact a part of a shortest path
  * Time: O( (V+E) \log{V} )
  */
 
@@ -14,12 +15,14 @@ void dijkstra(ll start){
     pq.push({0, start});
     d[start] = 0;
 
-    while( !pq.empty() ){
-        auto [p1, u] = pq.top(); pq.pop();
-        if (p1 > d[u]) continue;
-        for(auto [v, p2] : g[u]){
-            if (d[u] + p2 < d[v]){
-                d[v] = d[u] + p2;
+    while(!pq.empty()){
+
+        auto [cost, u] = pq.top(); pq.pop();
+        if (cost > d[u]) continue;
+
+        for (auto [v, w] : g[u]) {
+            if (d[u] + w < d[v]) {
+                d[v] = d[u] + w;
                 pq.push({d[v], v});
             }
         }
