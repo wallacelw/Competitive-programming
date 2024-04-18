56ccfa13adc08365b51c1ee27503ffa9e5a28164
Wallace <wallacebtlw@gmail.com>
2024-04-18 16:56:56 -0300

    cleaner version for suffix automaton

--- content/strings/suffix-automaton.cpp
+++ content/strings/suffix-automaton.cpp
@@ -7,78 +7,88 @@
  * Status: Tested in CSES
 */
 
-struct SuffixAutomaton {
+struct Automaton {
     struct State {
-        ll len = 0, link = 0; // link = the state id connected by the uplink
-        map<char, ll> down = {}; // ids of connected states below
- 
-        // extra for problems {p2, p3}
-        bool isClone = false;
-        ll first_pos = -1;
-        //
+        ll len = 0, link = 1;
+        map<char, ll> down = {}; // 0 => non existent edge
+        ll cnt = 0, fpos = -1;
+
+        ll& operator [](const char c) {
+            return down[c];
+        }
     };
  
-    ll n = 1; // number of states
+    ll n = 2; // number of states
     vector<State> ton; // short for automaton :D
+    vector<ll> order_len;
     string s;
- 
-    SuffixAutomaton(string ss) : s(ss) {
-        ton.pb( {0, -1} ); // root = 0
+    
+    Automaton(string ss) : s(ss) {
+        // root = 1, root.link = 0 (0 is a dummy node)
+        ton.assign(2, {0, 0});
+
         for(auto c : s) add(c);
+
+        for(ll i=1; i<n; i++) order_len.pb(i);
+        sort(order_len.begin(), order_len.end(), [&](auto l, auto r) {
+            return ton[l].len > ton[r].len;
+        });
+        for(auto i : order_len) {
+            ll j = ton[i].link;
+            if (j != 1) ton[j].cnt += ton[i].cnt;
+        }
     }
- 
-    ll last = 0; // last state's id
+
+    ll last = 1;
     void add(char c) {
-        ton.pb( {ton[last].len+1} );
         ll cur = n++;
-        
-        // extra for problems {p3}
-        ton[cur].first_pos = ton[cur].len - 1;
-        //
- 
         ll p = last;
-        while (p != -1 and !ton[p].down.count(c)) {
-            ton[p].down[c] = cur;
-            p = ton[p].link;
-        }
- 
-        if (p != -1) {
-            ll q = ton[p].down[c];
-            if (ton[p].len + 1 == ton[q].len) {
-                ton[cur].link = q;
-            } 
-            else {
-                ton.pb(ton[q]);
-                ll clone = n++;
-                ton[clone].len = ton[p].len + 1;
- 
-                // extra for problems {p2}
-                ton[clone].isClone = true;
-                //
- 
-                while (p != -1 and ton[p].down[c] == q) {
-                    ton[p].down[c] = clone;
-                    p = ton[p].link;
-                }
-                ton[cur].link = ton[q].link = clone;
-            }
-        }
         last = cur;
-    }
- 
-//// problems that can be solved ////
-// OBS: an additional time complexity of log{k} will be used when implementing with map
- 
+
+        State node; // state[cur]
+        node.len = ton[p].len + 1;
+        node.cnt = 1;
+        node.fpos = node.len - 1;
+        ton.pb(node);
+
+        for (;p and !ton[p].down.count(c); p = ton[p].link) 
+            ton[p][c] = cur;
+
+        if (p == 0) return;
+
+        ll q = ton[p][c];
+        if (ton[p].len + 1 == ton[q].len) {
+            ton[cur].link = q;
+            return;
+        }
+
+		ll clone = n++;
+        State node2 = ton[q]; // state[clone]
+        node2.cnt = 0;
+        node2.len = ton[p].len + 1;
+        ton.pb(node2);
+
+		ton[cur].link = ton[q].link = clone;
+
+		for (; ton[p][c] == q; p = ton[p].link)
+            ton[p][c] = clone;
+	}
+
+    // ----- //
+    // Tasks //
+    // ----- //
+
     // s1. Number of distinct substrings
     // separated in a vector by their lengths
     // using the property that: minlen(cur) = 1 + len(link(cur))
-    // and that state[cur] cover all the substrings 
-    // (suffixes) of size [minlen, len] represented by this state
+    // and that state[cur] cover all the substrings (suffixes) 
+    // of size [minlen, len] represented by this state
+
     vector<ll> distinctSubstrings() { // O(n)
         ll sz = s.size();
         vector<ll> ans(sz+1, 0);
  
-        for(ll i=1; i<n; i++) {
+        for(ll i=2; i<n; i++) {
             ll j = ton[i].link;
             ll minlen = 1 + ton[j].len;
             ll len = ton[i].len;
@@ -95,103 +105,102 @@ struct SuffixAutomaton {
         // ans[0] = 0, because the empty string is not considered as a substring
         return ans;
     }
- 
-    // s2. find the lexicographically k-th substring
-    // The k-th substring corresponds to the lexicographically  
-    // k-th path in the suffix automaton
- 
+
+    // s2. Find the lexicographically k-th substring (one can consider only the distincts or not)
+    // The k-th substring corresponds to the lexicographically smallest one,
+    // which is also the k-th path in the suffix automaton
+    
     // Additionally, by creating the automaton on the duplicated string (S+S),
     // the k-th substring with k = s.size(), will give us the Smallest cyclic shift (Minimal Rotation)
-    string substring(ll k) { // O(V+E) = O(2sz+ 3sz) = O(5sz), sz = s.size()
-        string ans = ""; // {k = 0} will return the empty string ""
-        ll idx = 0;
+    // This may have a big complexity nevertheless, need to optimize
 
-        // iterative dfs to avoid stack overflow
-        vector<bool> vis(n, 0);
-        stack<pair<ll, char>, vector<pair<ll, char>>> st;
-        st.push({0, 0}); // root
-        
-        while(st.size()) {
-            auto [u, concat] = st.top();
+    // number of substrings below node (including node)
+    // ps[0] -> include repeated substring, ps[1] -> consider only distinct
+    vector<ll> ps[2]; 
 
-            if (vis[u]) { // pop command 
-                ans.pop_back();
-                st.pop();
-                continue;
-            }
-            vis[u] = true;
-            
-            if (concat != 0) {
-                ans += concat; idx++;
-                if (idx == k) break;
-            } 
+    void buildPS() {
+        ps[0].assign(n, 0), ps[1].assign(n, 0);
 
-            auto edges = ton[u].down;
-            for(auto itr = edges.rbegin(); itr != edges.rend(); itr++) {
-                auto [c, v] = *itr;
-                st.push({v, c});
+        for(ll k : {0, 1}) {
+            for(auto i : order_len) {
+                if (i != 1) 
+                    ps[k][i] = (k ? 1 : ton[i].cnt);
+
+                for(auto [c, j] : ton[i].down) {
+                    ps[k][i] += ps[k][j];
+                }
             }
         }
+    }
 
+    string substring(ll k, bool distinct = true) { // O(V+E) = O(2sz+ 3sz) = O(5sz), sz = s.size()
+        assert(!ps[0].empty());
+ 
+        string ans = ""; // {k = 0} will return the empty string ""
+ 
+        function<void (ll)> dfs = [&](ll u) {
+            if (k <= 0) return;
+            for(auto [c, v] : ton[u].down) {
+                ll sum = ps[distinct][v];
+                if (k <= sum) {
+                    ans += c;
+                    k -= (distinct ? 1 : ton[v].cnt);
+                    dfs(v);
+                    if (k <= 0) return;
+                }
+                else k -= sum; // optimization
+            }
+        };
+ 
+        dfs(1);
         return ans;
     }
- 
- 
-/// Involving Patterns ///
- 
+
+
+    // -------- //
+    // Patterns //
+    // -------- //
+
     // p1. Check for occurrence of a pattern P
     // by returning the length of the longest prefix of P in S
-    ll checkPattern(string &p) { // O( p.size() )
-        ll ans = 0;
-        ll cur = 0;
+    // A match occurs when len(prefix_pattern) == len(pattern)
+
+    ll prefixPattern(string &p) { // O( p.size() )
+        ll ans = 0, cur = 1;
         for(auto c : p) {
             if (ton[cur].down.count(c)) {
-                cur = ton[cur].down[c];
+                cur = ton[cur][c];
                 ans += 1;
             }
             else break;
         }
         return ans;
     }
- 
+
     // p2. Count the numbers of occurrences of a pattern P
-    vector<ll> cnt;
-    void count() { // O(n log(n))
-        cnt.assign(n, 0);
-        vector<pll> order;
-        for(ll i=1; i<n; i++) {
-            order.pb({ton[i].len, i});
-            if (!ton[i].isClone)
-                cnt[i] = 1;
-        }
-        // sort by len decreasingly
-        sort(order.rbegin(), order.rend());
-        for(auto [len, i] : order) {
-            cnt[ton[i].link] += cnt[i];
-        }
-    }
+
     ll countPattern(string &p) { // O( p.size() )
-        assert(!cnt.empty());
-        ll cur = 0;
+        ll u = 1;
         for(auto c : p) {
-            if (ton[cur].down.count(c)) {
-                cur = ton[cur].down[c];
+            if (ton[u].down.count(c)) {
+                u = ton[u].down[c];
             }
             else return 0; // no match
         }
-        return cnt[cur];
+        return ton[u].cnt;
     }
- 
+
     // p3. Find the first position in which occurred the pattern (0-idx)
+
     ll firstPattern(string &p) { // O( p.size() )
-        ll cur = 0;
+        ll u = 1;
         for(auto c : p) {
-            if (ton[cur].down.count(c)) {
-                cur = ton[cur].down[c];
+            if (ton[u].down.count(c)) {
+                u = ton[u].down[c];
             }
             else return -1; // no match
         }
         ll sz = p.size();
-        return ton[cur].first_pos - sz + 1;
+        return ton[u].fpos - sz + 1;
     }
-}
+};
