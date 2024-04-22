4b93f74d009a841682369049077ede07d26362e0
Wallace <wallacebtlw@gmail.com>
2024-04-22 16:29:39 -0300

    treap fix

--- content/data-structures/treap.cpp
+++ content/data-structures/treap.cpp
@@ -3,12 +3,12 @@
  * Date: 22/04/2024
  * Description: Implicit Treap
  * Time: O(log{n}) with high probability
- * Status: Tested
+ * Status: Tested(but it may have memory leakage)
  */
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
-
-struct Treap { // Implicit
+ 
+struct Treap { // Implicit 0-idx
     struct Node {
         Node *l = NULL, *r = NULL;
         ll val, p;
@@ -41,15 +41,15 @@ struct Treap { // Implicit
             }
         }
     };
-
+ 
     Node* root;
-
+ 
     Treap() { root = NULL; }
-
+ 
     Treap(const Treap& t) { // copy constructor
         throw logic_error("Nao copiar a Treap!");
     }
-
+ 
     ~Treap() { // deconstructor
         vector<Node*> q = {root};
         while (q.size()) {
@@ -59,14 +59,15 @@ struct Treap { // Implicit
             delete x;
         }
     }
-
-	ll size(Node* x) { return x ? x->sz : 0; }
+ 
+    ll size(Node* x) { return x ? x->sz : 0; }
+    ll size() { return size(root); }
 
     // Supposes that l < r
-	void merge(Node*& x, Node* l, Node* r) { 
-		if (!l or !r) return void(x = l ? l : r);
-		l->prop(), r->prop();
-		if (l->p > r->p) {
+    void merge(Node*& x, Node* l, Node* r) { 
+        if (!l or !r) return void(x = l ? l : r);
+        l->prop(), r->prop();
+        if (l->p > r->p) {
             merge(l->r, l->r, r);
             x = l;
         }
@@ -75,14 +76,14 @@ struct Treap { // Implicit
             x = r;
         }
         x->update();
-	}
-
+    }
+ 
     // split into [0, mid), [mid, n)
     // with size(left) = mid, size(right) = n-mid
-	void split(Node* x, Node*& l, Node*& r, ll mid) {
-		if (!x) return void(r = l = NULL);
-		x->prop();
-		if (size(x->l) < mid) {
+    void split(Node* x, Node*& l, Node*& r, ll mid) {
+        if (!x) return void(r = l = NULL);
+        x->prop();
+        if (size(x->l) < mid) {
             split(x->r, x->r, r, mid - size(x->l) - 1);
             l = x;
         }
@@ -91,44 +92,45 @@ struct Treap { // Implicit
             r = x;
         }
         x->update();
-	}
-
+    }
+ 
     // insert new element with val=v into the rightmost position
-	void insert(ll v) {
-		Node* x = new Node(v);
-		merge(root, root, x);
-	}
-
+    void insert(ll v) {
+        Node* x = new Node(v);
+        merge(root, root, x);
+    }
+ 
     // get the query value for [l, r]
-	ll query(ll l, ll r) {
-		Node *L, *M, *R;
-		split(root, M, R, r+1), split(M, L, M, l);
-		ll ans = M->sum;
-		merge(M, L, M), merge(root, M, R);
-		return ans;
-	}
-
+    ll query(ll l, ll r) {
+        Node *L, *M, *R;
+        split(root, M, R, r+1), split(M, L, M, l);
+        ll ans = M->sum;
+        merge(M, L, M), merge(root, M, R);
+        return ans;
+    }
+ 
     // increment value for [l, r]
-	void increment(ll l, ll r, ll s) {
-		Node *L, *M, *R;
-		split(root, M, R, r+1), split(M, L, M, l);
-		M->lazy += s;
-		merge(M, L, M), merge(root, M, R);
-	}
-
+    void increment(ll l, ll r, ll s) {
+        Node *L, *M, *R;
+        split(root, M, R, r+1), split(M, L, M, l);
+        M->lazy += s;
+        merge(M, L, M), merge(root, M, R);
+    }
+ 
     // reverses interval [l, r] to [r, l]
-	void reverse(ll l, ll r) {
-		Node *L, *M, *R;
-		split(root, M, R, r+1), split(M, L, M, l);
-		M->rev ^= 1;
-		merge(M, L, M), merge(root, M, R);
-	}
+    void reverse(ll l, ll r) {
+        Node *L, *M, *R;
+        split(root, M, R, r+1), split(M, L, M, l);
+        M->rev ^= 1;
+        merge(M, L, M), merge(root, M, R);
+    }
 };
-
+ 
 vector<ll> dfs(Treap::Node *root) {
     vector<ll> ans;
     function<void (Treap::Node*)> print = [&](Treap::Node *u) {
         if (!u) return;
+        u->prop();
         print(u->l);
         ans.pb(u->val);
         print(u->r);
