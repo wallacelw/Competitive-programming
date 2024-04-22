3357565fa9d058bad87da444fd920a1525683aad
Wallace <wallacebtlw@gmail.com>
2024-04-22 17:00:01 -0300

    fixing treap memory leakage

--- content/data-structures/treap.cpp
+++ content/data-structures/treap.cpp
@@ -3,7 +3,7 @@
  * Date: 22/04/2024
  * Description: Implicit Treap
  * Time: O(log{n}) with high probability
- * Status: Tested(but it may have memory leakage)
+ * Status: Tested(but it can be improved and the increment() function is not tested yet)
  */
 
 mt19937 rng(chrono::steady_clock::now().time_since_epoch().count());
@@ -17,7 +17,7 @@ struct Treap { // Implicit 0-idx
         Node(ll v) : val(v), p(rng()) {
             sz = 1, sum = val, lazy = 0;
         }
-        void prop() {
+        void push() {
             if (lazy) {
                 val += lazy, sum += lazy*sz;
                 if (l) l->lazy += lazy;
@@ -34,7 +34,7 @@ struct Treap { // Implicit 0-idx
             sz = 1, sum = val;
             for(auto x : {l, r}) {
                 if (x) {
-                    x->prop();
+                    x->push();
                     sz += x->sz;
                     sum += x->sum;
                 }
@@ -46,7 +46,8 @@ struct Treap { // Implicit 0-idx
  
     Treap() { root = NULL; }
  
-    Treap(const Treap& t) { // copy constructor
+    // copy constructor to remind the user to not copy the treap object
+    Treap(const Treap& t) { 
         throw logic_error("Nao copiar a Treap!");
     }
  
@@ -61,12 +62,12 @@ struct Treap { // Implicit 0-idx
     }
  
     ll size(Node* x) { return x ? x->sz : 0; }
-    ll size() { return size(root); }
+    ll size() { return size(root); } // maybe useless line of code
 
-    // Supposes that l < r
+    // Supposes that l < r when merging
     void merge(Node*& x, Node* l, Node* r) { 
         if (!l or !r) return void(x = l ? l : r);
-        l->prop(), r->prop();
+        l->push(), r->push();
         if (l->p > r->p) {
             merge(l->r, l->r, r);
             x = l;
@@ -82,7 +83,7 @@ struct Treap { // Implicit 0-idx
     // with size(left) = mid, size(right) = n-mid
     void split(Node* x, Node*& l, Node*& r, ll mid) {
         if (!x) return void(r = l = NULL);
-        x->prop();
+        x->push();
         if (size(x->l) < mid) {
             split(x->r, x->r, r, mid - size(x->l) - 1);
             l = x;
@@ -109,7 +110,7 @@ struct Treap { // Implicit 0-idx
         return ans;
     }
  
-    // increment value for [l, r]
+    // increment value for [l, r] (not tested yet)
     void increment(ll l, ll r, ll s) {
         Node *L, *M, *R;
         split(root, M, R, r+1), split(M, L, M, l);
@@ -124,17 +125,19 @@ struct Treap { // Implicit 0-idx
         M->rev ^= 1;
         merge(M, L, M), merge(root, M, R);
     }
-};
- 
-vector<ll> dfs(Treap::Node *root) {
-    vector<ll> ans;
-    function<void (Treap::Node*)> print = [&](Treap::Node *u) {
+
+    // return in a vector all the elements in the treap, from left to right
+    void inOrder(Node *u, vector<ll> &vec) {
         if (!u) return;
-        u->prop();
-        print(u->l);
-        ans.pb(u->val);
-        print(u->r);
-    };
-    print(root);
-    return ans;
-}
+        u->push();
+        // in-order
+        inOrder(u->l, vec);
+        vec.pb(u->val);
+        inOrder(u->r, vec);
+    }
+    vector<ll> get() {
+        vector<ll> vec;
+        inOrder(root, vec);
+        return vec;
+    }
+};
