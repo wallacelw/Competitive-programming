a9fbb9ad1a44e7499a64f562f9b1f13728c00b51
wallace <wallacebtlw@gmail.com>
2024-06-07 15:54:44 -0300

    hierholzer undirected path

--- content/graph/hierholzer.cpp
+++ content/graph/hierholzer.cpp
@@ -3,7 +3,7 @@
  * Date: 06/06/2024
  * Description: Check existence conditions and produce path if possible
  * Time: O(E)
- * Status: Tested in CSES
+ * Status: Tested in CSES, cf(https://codeforces.com/contest/1981/my)
  */
 
 // Euler Circuit - Undirected Graph (1-idx)
@@ -39,6 +39,44 @@ pair<bool, vector<ll>> circuit(ll n, vector<pll> &edges, ll src = 1) {
     // check for connectivity
     if (ans.size() != m + 1) return {false, {}};
 
+    return {true, ans};
+}
+
+// Euler Path - Undirected Graph (1-idx)
+pair<bool, vector<ll>> path(ll n, vector<pll> &edges, ll src, ll dst) { 
+    ll m = edges.size();
+    vector<vector<pll>> g(n+1);
+    
+    for(ll i=0; i<m; i++) {
+        auto [a, b] = edges[i]; 
+        g[a].pb({b, i});
+        g[b].pb({a, i});
+    }
+
+    // check for even degree (except src and dst)
+    for(ll i=0; i<=n; i++) {
+        if (i == src or i == dst) continue;
+        if (g[i].size() % 2) return {false, {}};
+    }
+
+    vector<ll> ans;
+    vector<bool> vis(m, 0);
+    function<void (ll)> dfs = [&](ll u) {
+        while(g[u].size()) {
+            auto [v, idx] = g[u].back();
+            g[u].pop_back();
+            if (vis[idx]) continue;
+            vis[idx] = 1;
+            dfs(v);
+        }
+        ans.pb(u);
+    };
+    dfs(src);
+
+    // check for connectivity
+    if (ans.size() != m + 1) return {false, {}};
+
+    reverse(ans.begin(), ans.end());
     return {true, ans};
 }
 
