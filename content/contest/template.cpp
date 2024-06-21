ed628a74c37e8ed2d4f080af9c37b0190a6cd107
Wallace <wallacebtlw@gmail.com>
2024-06-21 20:33:32 -0300

    notebook 21-06-2024

--- content/contest/template.cpp
+++ content/contest/template.cpp
@@ -4,23 +4,34 @@
 using namespace std;
 #define sws cin.tie(0)->sync_with_stdio(0)
 
+typedef long long ll;
+typedef long double ld;
 #define endl '\n'
-#define ll long long
-#define ld long double
 #define pb push_back
 #define ff first
 #define ss second
 #define pll pair<ll, ll>
 #define vll vector<ll>
+
 #define teto(a, b) (((a)+(b)-1)/(b))
+#define LSB(i) ((i) & -(i))
+#define MSB(i) (63 - __builtin_clzll(i))
+#define BITS(i) __builtin_popcountll(i)
 
-template<class A>void db(A a){for(auto b:a){cout<<b<<" ";}cout<<endl;}
-template<class A>void debug(A a){for(auto b:a)db(b);}
-template<class... A>void dbg(A const&...a){((cout<<"{"<<a<<"} "),...);cout<<endl;}
+template<class A, class B> auto& operator<<(ostream &os, pair<A, B> p) { 
+    return os << '{' << p.ff << ", " << p.ss << '}'; }
+template<class... A>void db(A ...a) {((cerr<<"{"<<a<<"} "), ...); cerr<<endl;}
+template<class A>void db2(A a) {for(auto b:a) {cerr<<b<<" ";} cerr<<endl;}
+template<class A>void db3(A a) {for(auto b:a) db2(b);}
+#define dbg(...) {cerr << #__VA_ARGS__ << ": "; db(__VA_ARGS__); }
+#define deb(a) {cerr << #a << ": "; db2(a); }
+#define debug(a) {cerr << #a << ": " << endl; db3(a); }
 
-const ll MAX = 1e6+10;
-const ll MOD = 1e9 + 7;
-const ll INF = INT64_MAX;
+const ll MAX = 2e5+10;
+const ll MOD = 998'244'353;
+const ll INF = 1e18; // INT32_MAX
+const ld EPS = 1e-7;
+const ld PI = acos(-1);
 
 #include <chrono>
 using namespace std::chrono;
