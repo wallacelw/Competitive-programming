8a76cf38223acb65c045e0a1bbb25a8cd69255aa
wallace <wallacebtlw@gmail.com>
2024-07-08 14:09:03 -0300

    trie update

--- content/data-structures/trie.cpp
+++ content/data-structures/trie.cpp
@@ -1,49 +1,30 @@
 /**
  * Author: Wallace, Tiago
- * Date: 11/03/2024
+ * Date: 08/07/2024
  * Description: Creates a trie by pre-allocating the trie array, which contains the indices for the child nodes.
  * The trie can be easily modified to support alphanumeric strings instead of binary strings.
  * Time: O(D), D = depth of trie
- * Status: Tested (https://cses.fi/problemset/result/5250973/)
+ * Status: Tested (https://cses.fi/problemset/result/9792405/)
  */
 
-// MAX = maximum number of nodes that can be created
+ll alphabet = 26;
 
-struct Trie{
-    ll trie[MAX][26];
-    bool isWordEnd[MAX];
-    ll nxt = 1, wordsCnt = 0;
- 
+struct Trie {
+    vector<vll> t;
+    vector<ll> words;
+    ll idx = 1;
+    
+    // n = maximum number of nodes to be created = N*len
+    Trie(ll n) : t(n, vll(alphabet, 0)), words(n, 0) {}
+
     void add(string s){ // O(Depth)
         ll node = 0;
-        for(auto c: s) {
-            if(trie[node][c-'a'] == 0) { // create new node
-                trie[node][c-'a'] = nxt++;
+        for(auto c : s) {
+            if(t[node][c-'a'] == 0) { // create new node
+                t[node][c-'a'] = idx++;
             }
-            node = trie[node][c-'a'];
+            node = t[node][c-'a'];
         }
-        if(!isWordEnd[node]){
-            isWordEnd[node] = true;
-            wordsCnt++;
-        }
-    }
- 
-    bool find(string s, bool remove=false){ // O(Depth)
-        ll node = 0;
-        for(auto c: s) {
-            if(trie[node][c-'a'] == 0) {
-                return false;
-            }
-            else {
-                node = trie[node][c-'a'];
-            }
-        }
- 
-        if(remove and isWordEnd[node]){
-            isWordEnd[node] = false;
-            wordsCnt--;
-        }
-
-        return isWordEnd[node];
+        words[node]++;
     }
 };
