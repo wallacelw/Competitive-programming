6dda232f261dab0885ea4572f10ff2e2d1a57eb9
wallace <wallacebtlw@gmail.com>
2024-07-15 10:19:04 -0300

    extended euclid refactor

--- content/math/extended-euclid.cpp
+++ content/math/extended-euclid.cpp
@@ -15,9 +15,9 @@ ll extendedEuclid(ll a, ll b, ll &x, ll &y) {
     ll x1 = 0, y1 = 1, a1 = a, b1 = b;
     while (b1) {
         ll q = a1 / b1;
-        tie(x, x1) = pll{x1, x - q * x1};
-        tie(y, y1) = pll{y1, y - q * y1};
-        tie(a1, b1) = pll{b1, a1 - q * b1};
+        tie(x, x1) = pair{x1, x - q * x1};
+        tie(y, y1) = pair{y1, y - q * y1};
+        tie(a1, b1) = pair{b1, a1 - q * b1};
     }
     return a1;
 }
