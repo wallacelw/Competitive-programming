5cb43259e2ffc6b8def06e6b7c52b27cee0f0e72
wallace <wallacebtlw@gmail.com>
2024-09-18 17:56:40 -0300

    segment tree sparse

--- content/data-structures/seg-sparse.cpp
+++ content/data-structures/seg-sparse.cpp
@@ -1,81 +1,61 @@
-// doesnt work yet
+/**
+ * Author: Wallace
+ * Date: 18/09/2024 
+ * Description: Sparse Segment Tree, for range sum query, point point increase
+ * When initializing, choose an appropriate value for (n, q and mx)
+ * Time: O(\log(N)) to update or query
+ * Status: Tested {https://cses.fi/problemset/result/10556687/}
+ */
+
+// Sparse Segtree that covers [0, mx]
+// n = initial vector size, q = number of queries
+// point add update, range sum query
 struct Segtree {
     struct Node {
-        ll sum = 0;
-
         ll l = 0, r = 0;
+        ll sum = 0;
     };
 
-    ll L = 0, R;
-    // 0=null, 1=root
+    // t[0]=null, t[1]=root
+    ll idx = 2, L = 0, R;
     vector<Node> t; 
 
-    Segtree(ll n) : R(n), t(2) {}
+    Segtree(ll mx, ll n=1e6, ll q=1e6) : R(mx), t((n+q+10)*__lg(2*n+10)) {}
 
-    Node merge(Node a, Node b) {
-        return Node{a.sum + b.sum};
-    }
-
-    void expand(Node &node) {
-        node.l = t.size();
-        t.pb(Node{});
-        node.r = t.size();
-        t.pb(Node{});
+    ll merge(ll a, ll b) {
+        return a + b;
     }
 
     void add(ll pos, ll val, ll l, ll r, ll i) {
-        if (l == r) {
+        if (l == r) {    
             t[i].sum += val;
             return;
         }
         ll mid = (l+r)/2;
-        expand(t[i]);
-        if (pos <= mid)
+        if (pos <= mid) {
+            if (!t[i].l) t[i].l = idx++;
             add(pos, val, l, mid, t[i].l);
-        else
+        }
+        else {
+            if (!t[i].r) t[i].r = idx++;
             add(pos, val, mid+1, r, t[i].r);
-        t[i] = merge(t[t[i].l], t[t[i].r]);
+        }
+        t[i].sum = merge(t[t[i].l].sum, t[t[i].r].sum);
     }
     void add(ll pos, ll val) {
         add(pos, val, L, R, 1);
     }
 
-    Node query(ll a, ll b, ll l, ll r, ll i) {
-        if (b < l or r < b) return Node{};
-        if (b <= l and r <= b) return t[i];
+    ll query(ll a, ll b, ll l, ll r, ll i) {
+        if (b < l or r < a) return 0;
+        if (a <= l and r <= b) return t[i].sum;
         ll mid = (l+r)/2;
-        return merge(
-            query(a, b, l, mid, t[i].l),
-            query(a, b, mid+1, r, t[i].r)
-        );
+        ll ansl = 0, ansr = 0;
+        if (t[i].l) ansl = query(a, b, l, mid, t[i].l);
+        if (t[i].r) ansr = query(a, b, mid+1, r, t[i].r);
+        return merge(ansl, ansr);
     }
-    Node query(ll a, ll b) {
+    ll query(ll a, ll b) {
         return query(a, b, L, R, 1);
     }
 };
-
-int32_t main() { sws;
-    ll n, q; cin >> n >> q;
-
-    vector<ll> x(n+1);
-    Segtree st(1e10);
-
-    for(ll i=1; i<=n; i++) {
-        cin >> x[i];
-        st.add(x[i], 1);
-    }
-
-    while(q--) {
-        char c; cin >> c;
-        if (c == '!') {
-            ll pos, val; cin >> pos >> val;
-            st.add(x[pos], -1);
-            x[pos] = val;
-            st.add(x[pos], 1);
-        }
-        else {
-            ll a, b; cin >> a >> b;
-            cout << st.query(a, b).sum << endl;
-        }
-    }
-}
